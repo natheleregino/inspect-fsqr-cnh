e19eb073345c387c7404c527b3cb38eb4b0d54b0
natheleregino <natheleregino@gmail.com>
2026-05-21 13:25:36 -0300

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "861840456" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "232868020" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
